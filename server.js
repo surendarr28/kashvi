2956633e5f4422e4ecb99c9f4dea38c6ba3e49c5
SurendarR <surendar.r@tataelxsi.co.in>
2019-09-01 16:28:04 +0200

    files added

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var path = require('path');
 var cors = require('cors');
 var app = express();
 
-app.use(express.static(path.resolve(__dirname, 'app/build')));
-console.log(path.resolve(__dirname, 'app/build'));
+app.use(express.static(path.resolve(__dirname, 'smartkiddiz')));
+console.log(path.resolve(__dirname, 'smartkiddiz'));
 
 app.get('/', function (req, res) {
-   res.sendFile(path.resolve(__dirname, 'app/build/index.html'));
+   res.sendFile(path.resolve(__dirname, 'smartkiddiz/index.html'));
 })
 app.get('/mp31', function (req, res) {
     res.sendFile(path.resolve(__dirname, 'kplay_back1.mp3'));
  })
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Example app listening on port 3000!')
 })
